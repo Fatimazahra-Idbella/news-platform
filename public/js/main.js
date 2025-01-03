b6c98c5aa5bcc7c6627cb85dd1b8284bcfa9255d
fatima ezzahra <adblafatmtalzhra@gmail.com>
2025-01-03 11:28:33 +0100

    Compléter la fonction displayNews

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,28 @@ async function fetchLatestNews() {
 function displayNews(news) {
     const container = document.getElementById('news-container');
     // Utilisez Bootstrap pour créer des cards pour chaque article
+    container.innerHTML = ''; // Vider le conteneur avant d'ajouter les articles
+
+    articles.forEach(article => {
+        const card = document.createElement('div');
+        card.className = 'card mb-3';
+
+        card.innerHTML = `
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="${article.image}" class="card-img" alt="${article.title}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${article.title}</h5>
+                        <p class="card-text">${article.body.substring(0, 100)}...</p>
+                        <a href="#" class="btn btn-primary">Voir plus</a>
+                    </div>
+                </div>
+            </div>
+        `;
+        container.appendChild(card);
+    });
 }
 
 // TODO: Question 2 - Créer une fonction pour gérer les erreurs
